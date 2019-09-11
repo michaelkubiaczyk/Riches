fc9d363e9ab013147239f7a3f86096e725ad6a9b
michaelkubiaczyk <48311127+michaelkubiaczyk@users.noreply.github.com>
2019-09-11 14:17:17 +0200

    Update scan.bat

--- riches/scan.bat
+++ riches/scan.bat
@@ -9,3 +9,5 @@ ant clean
 echo Translating and Scanning...
 ant all
 cxsast -findbugs -cp "jsplibs/**/*.jar;lib/**/*.jar;WEB-INF/lib/**/*.jar" -source 1.5 -java-build-dir "WEB-INF/classes" WEB-INF/ js/ jsplibs/ login/ pages/ config/ -f scanResults.fpr
+
+echo HERPADERP.
